3991e2dbb92f4f4370a704a17c5596ce456a0907
Rex Ye <rxye1991@gmail.com>
2020-10-11 17:22:22 -0400

    createdLanindgpAg

--- src/Components/LandingPage/Landing.js
+++ src/Components/LandingPage/Landing.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Landing() {
     return (
         <div>
-            
+                <h4> Landing Page, Render Login/Sign Up and info about this app.</h4>
         </div>
     )
 }
